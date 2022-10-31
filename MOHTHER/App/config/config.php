3d8f70a8dfdd204a4de461da236896920526f4c4
amanividanage <amanividanage@gmail.com>
2022-10-31 23:25:43 +0530

    Initial
    Framework

---
+++ MOHTHER/App/config/config.php
@@ -0,0 +1,7 @@
+<?php
+//App root
+define('APPROOT', dirname(dirname(__FILE__)));
+//URL Root
+define('URLROOT', 'http://localhost/MOHTHER');
+//Site Name
+define('SITENAME', 'MOHTHER');
---
+++ MOHTHER/public/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once '../app/bootstrap.php';# outside the public folder and then into the app folder
+
+//init core library
+$init = new Core;
